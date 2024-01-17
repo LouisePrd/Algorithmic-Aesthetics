bb1a4e07f6502db7848bd50b8295b1577083544b
LouisePrd <louise.peridy22@gmail.com>
2024-01-17 15:43:12 +0100

    fix homepage, change name and add ex3

--- exercice-2/ex2.js
+++ exercice-2/ex2.js
@@ -1,7 +1,7 @@
 // 4 mots : rêves, chauchemars, sommeil, éveil
 
 /* Description :
-Ce petit programme génère une grille de 10x10 carrés de 60px de côté. Au départ, les couleurs sont pastel, douces et agréable.
+Ce petit programme génère une grille de 10x10 carrés de 60px de côté. Au départ, les couleurs sont pastelssssss, douces et agréablesssssss.
 Elles rassurent, comme un rêve. Petit à petit, les couleurs vont devenir plus sombres, plus agressives, comme si le rêve passait au cauchemar.
 On passe donc du sommeil profond et confortable à l'éveil brutal et désagréable. */
 
@@ -12,9 +12,8 @@ function setup() {
 
 function draw() {
   if (frameCount % 15 == 0) {
-    background(255 - frameCount/2 , 105 - frameCount/3 , 115, 127);
+    background(255 - frameCount / 2, 105 - frameCount / 3, 115, 127);
     createColorPalette();
-    
   }
 }
 
---
+++ exercice-3/ex3.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(500, 500);
+  background(230);
+  createGrid();
+}
+
+function draw() {}
+
+function createGrid() {
+    let gap = 30;
+    square(gap, gap, width - 2*gap);
+}
+
+function addElements() {
+    let gap = 30;
+    for (i < gap; i++; i < width - gap) {
+        for (j < gap; j++; j < height - gap) {
+
+        }
+    }
+}
