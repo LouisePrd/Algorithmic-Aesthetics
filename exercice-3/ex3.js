5a20a04c31e523372fe1633d67d38351402171f1
LouisePrd <louise.peridy22@gmail.com>
2024-01-17 22:19:33 +0100

    fix randomX ex3

--- exercice-3/ex3.js
+++ exercice-3/ex3.js
@@ -6,31 +6,50 @@ function setup() {
 
 function draw() {}
 
+// creation de grille avec les marges mais 
+// enlevée car problèmes de dépassement des carrés
 function createGrid() {
   let gap = 50;
   square(gap, gap, width - 2 * gap);
 }
 
 function addSquares() {
-  let gapRandomX = 200; // hauteur
-  let gapRandomY = 120; // longueur
+  let gapRandomX = 160; // longueur
+  let gapRandomY = 350; // hauteur
   let randomHeight = 0;
   let randomWidth = 0;
+  let isBlack = false;
   noStroke();
 
   for (let i = 0; i < 500; i++) {
     for (let j = 0; j < 500; j++) {
-      randomHeight = Math.floor(Math.random() * gapRandomY);
-      randomWidth = Math.floor(Math.random() * gapRandomX);
-      if (j % 2 == 0 && i % 2 == 0) {
+      randomHeight = Math.floor(Math.random() * gapRandomX);
+      randomWidth = Math.floor(Math.random() * gapRandomY);
+
+      // alternance verticale
+      if (isBlack == false) {
         fill("#231E1A");
+        isBlack = true;
       } else {
         fill("#E3DAC9");
+        isBlack = false;
       }
 
+      // 2 ranges car dans le dessin ceux du milieu 
+      // semblent plus grands horizontalement
+      if (j > 150 || j > 275) {
+        randomHeight = Math.floor((Math.random() * gapRandomX) / 2);
+        randomWidth = Math.floor((Math.random() * gapRandomY) / 2);
+      }
+
+      // plus on descend, plus les carrés sont petits
+      if (i > 150) {
+        randomHeight = Math.floor((Math.random() * gapRandomX) / 2);
+        randomWidth = Math.floor((Math.random() * gapRandomY) / 2);
+      }
       rect(j, i, j + randomWidth, i + randomHeight);
       j += randomWidth;
     }
-    i += 11;
+    i += 12; // problème ici car i fixe alors qu'il doit dépendre du carré précédent
   }
 }
