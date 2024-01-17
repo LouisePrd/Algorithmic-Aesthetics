b05be3bb732ad5aa51abe95d7884a6aa6ba43d1e
LouisePrd <louise.peridy22@gmail.com>
2024-01-17 20:55:47 +0100

    add start ex3

--- exercice-3/ex3.js
+++ exercice-3/ex3.js
@@ -1,21 +1,37 @@
 function setup() {
   createCanvas(500, 500);
-  background(230);
+  background("#E3DAC9");
   createGrid();
+  addSquares();
 }
 
 function draw() {}
 
 function createGrid() {
-    let gap = 30;
-    square(gap, gap, width - 2*gap);
+  let gap = 50;
+  square(gap, gap, width - 2 * gap);
 }
 
-function addElements() {
-    let gap = 30;
-    for (i < gap; i++; i < width - gap) {
-        for (j < gap; j++; j < height - gap) {
+function addSquares() {
+  let gapRandomX = 100;
+  let gapRandomY = 50;
+  let randomHeight = 0;
+  let randomWidth = 0;
+  noStroke();
 
-        }
+  // on divise l'image en 200 carrés de 10x10
+  for (let i = 50; i < 250; i++) {
+    for (let j = 50; j < 250; j++) {
+      randomHeight = Math.floor(Math.random() * gapRandomY);
+      randomWidth = Math.floor(Math.random() * gapRandomX);
+      if (i % 2 == 0) {
+        fill("#231E1A");
+      } else {
+        fill("#E3DAC9");
+      }
+      rect(i, j, randomWidth, randomHeight);
+      i += randomWidth;
+      j += randomHeight;
     }
+  }
 }
