f7d5b8e7affdb66f502c9b55d4b08efd8e88ea64
LouisePrd <louise.peridy22@gmail.com>
2024-01-17 21:45:25 +0100

    fix grid

--- exercice-3/ex3.js
+++ exercice-3/ex3.js
@@ -1,7 +1,6 @@
 function setup() {
   createCanvas(500, 500);
   background("#E3DAC9");
-  createGrid();
   addSquares();
 }
 
@@ -13,25 +12,25 @@ function createGrid() {
 }
 
 function addSquares() {
-  let gapRandomX = 100;
-  let gapRandomY = 50;
+  let gapRandomX = 200; // hauteur
+  let gapRandomY = 120; // longueur
   let randomHeight = 0;
   let randomWidth = 0;
   noStroke();
 
-  // on divise l'image en 200 carrés de 10x10
-  for (let i = 50; i < 250; i++) {
-    for (let j = 50; j < 250; j++) {
+  for (let i = 0; i < 500; i++) {
+    for (let j = 0; j < 500; j++) {
       randomHeight = Math.floor(Math.random() * gapRandomY);
       randomWidth = Math.floor(Math.random() * gapRandomX);
-      if (i % 2 == 0) {
+      if (j % 2 == 0 && i % 2 == 0) {
         fill("#231E1A");
       } else {
         fill("#E3DAC9");
       }
-      rect(i, j, randomWidth, randomHeight);
-      i += randomWidth;
-      j += randomHeight;
+
+      rect(j, i, j + randomWidth, i + randomHeight);
+      j += randomWidth;
     }
+    i += 11;
   }
 }
